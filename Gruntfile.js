ffc9a0fcd30aece27ec0d03b26a263eabce38324
Andrea Sonny <andreasonny83@gmail.com>
2016-02-02 21:54:33 +0000

    grunt task improved

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,33 @@ module.exports = function(grunt) {
   'use strict';
 
   grunt.initConfig({
+    clean: {
+      build: ['build'],
+    },
+
     webfont: {
       icons: {
         src: 'src/icons/*.svg',
-        dest: 'build/fonts'
+        dest: 'build/sonny-webfonts',
+        destCss: 'build',
+        options: {
+          hashes: false,
+          font: 'sonny-webfonts',
+          types: 'eot,woff2,woff,ttf,svg',
+          templateOptions: {
+            baseClass: 'sonny-icon',
+            classPrefix: 'sonny_',
+            mixinPrefix: 'sonny-'
+          },
+          htmlDemoFilename: 'demo',
+          destHtml: 'build'
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-webfont');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['webfont']);
+  grunt.registerTask('default', ['clean', 'webfont']);
   };
---
+++ README.md
@@ -0,0 +1,11 @@
+# SonnY-WebFont
+## SVG to webfont converter using grunt-webfont
+
+## Installation
+
+This plugin requires Grunt 0.4. Note that ttfautohint is optional, but your generated font will not be properly hinted if it’s not installed. And make sure you don’t use ttfautohint 0.97 because that version won’t work.
+
+    brew install ttfautohint fontforge --with-python
+    npm install
+
+Put all the source icon files in `src/icons` and run `grunt` for generating an iconfont inside the `build` folder
